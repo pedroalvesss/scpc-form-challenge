c099fd193dc6e6fcc7bdfbb94acbc7b3336ed02b
pedroalvesss <pedropetros1@gmail.com>
2025-04-09 13:22:21 -0300

    style: add stylization to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center mt-5">
-      <h1 className="font-bold text-3xl">Página Home</h1>
+      <h1 className="font-bold">H1</h1>
+
+      <div
+        id="FormSpace"
+        className="bg-discord w-180 h-200 flex flex-col items-center shadow-2xl"
+      ></div>
     </div>
   );
 }
