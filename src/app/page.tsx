af88cf702c06a38bd1ec4d03d9c1698f7f59737e
pedroalvesss <pedropetros1@gmail.com>
2025-04-10 16:07:56 -0300

    feat: add select and input file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm();
+  const form = useForm();
 
   const handleFormSubmit = (data) => {
-    console.log(data);
+    alert(JSON.stringify(data));
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
+    <div className="w-full h-screen flex flex-col justify-center items-center mt-5">
       <div
         id="FormSpace"
         className="bg-discord w-180 h-200 flex flex-col items-center shadow-2xl rounded-md"
@@ -23,29 +35,92 @@ export default function Home() {
         <h1 className="mt-5 text-4xl font-bold text-amber-50">
           Formulário TOP 2000
         </h1>
+
         <div className="flex flex-col mt-5">
-          <form
-            onSubmit={handleSubmit(handleFormSubmit)}
-            className="flex w-full max-w-lg flex-col items-center gap-4"
-          >
-            <input
-              {...register("userName")}
-              type="text"
-              placeholder="Nome"
-              className="bg-white h-10 w-60 rounded-md px-3 py-1"
-            />
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleFormSubmit)}
+              className="flex w-full max-w-lg flex-col items-center gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="teste"
+                render={({ field }) => (
+                  <FormItem className="w-100">
+                    <FormLabel className="text-white">Teste</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="text-white"
+                        type="text"
+                        placeholder="Exemplo"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <input
-              {...register("Teste")}
-              type="text"
-              placeholder="Teste"
-              className="bg-white h-10 w-60 rounded-md px-3 py-1"
-            />
+              <FormField
+                control={form.control}
+                name="teste2"
+                render={({ field }) => (
+                  <FormItem className="w-100">
+                    <FormLabel className="text-white">Teste</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="text-white"
+                        type="text"
+                        placeholder="Exemplo"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <Button type="submit" className="h-auto w-30 font-bold">
-              Enviar
-            </Button>
-          </form>
+              <FormField
+                control={form.control}
+                name="SelectDiretoria"
+                render={({ field }) => (
+                  <FormItem className="w-100">
+                    <FormLabel className="text-white w-full">
+                      Diretoria
+                    </FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="text-white w-100">
+                          <SelectValue placeholder="Selecione a diretoria" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="Diretoria do xaropinho">
+                          Diretoria do xaropinho
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+              <div className="">
+                <Label className="text-white mb-2" htmlFor="picture">
+                  Certificado
+                </Label>
+                <Input
+                  id="certificado"
+                  type="file"
+                  className="text-white w-100"
+                />
+              </div>
+
+              <Button type="submit" className="w-full">
+                Enviar
+              </Button>
+            </form>
+          </Form>
         </div>
       </div>
     </div>
