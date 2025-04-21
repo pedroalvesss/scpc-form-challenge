eb86668c6df2bb18888e30bb1ba193a5b1d5af10
pedroalvesss <pedropetros1@gmail.com>
2025-04-21 16:40:41 -0300

    refactor: change date validation

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,17 +1,28 @@
 import { z } from "zod";
 
-export const formSchema = z.object({
-  diretoria: z.string().min(1, "Campo obrigatório"),
-  areaConhecimento: z.string().min(1, "Campo obrigatório"),
-  tipo: z.string().min(1, "Campo obrigatório"),
-  modalidade: z.string().min(1, "Campo obrigatório"),
-  tituloEvento: z.string().min(1, "Campo obrigatório"),
-  cargaHorariaEstimada: z.string().min(1, "Campo obrigatório"),
-  nomeInstituicao: z.string().min(1, "Campo obrigatório"),
-  inicioCurso: z.date({ required_error: "Selecione uma data de início" }),
-  finalCurso: z.date({ required_error: "Selecione uma data de fim" }),
-  dataExpedido: z.date({ required_error: "Selecione a data de expedição" }),
-  certificado: z.instanceof(File, { message: "Envie um arquivo válido" }),
-});
+export const formSchema = z
+  .object({
+    diretoria: z.string().min(1, "Campo obrigatório").optional(),
+    areaConhecimento: z.string().min(1, "Campo obrigatório"),
+    tipo: z.string().min(1, "Campo obrigatório"),
+    modalidade: z.string().min(1, "Campo obrigatório"),
+    tituloEvento: z.string().min(1, "Campo obrigatório"),
+    cargaHorariaEstimada: z.string().min(1, "Campo obrigatório"),
+    nomeInstituicao: z.string().min(1, "Campo obrigatório"),
+    inicioCurso: z.date({ required_error: "Selecione uma data de início" }),
+    finalCurso: z.date({ required_error: "Selecione uma data de fim" }),
+    dataExpedido: z.date({ required_error: "Selecione a data de expedição" }),
+    certificado: z
+      .instanceof(File, { message: "Envie um arquivo válido" })
+      .optional(),
+  })
+  .refine((data) => data.finalCurso >= data.inicioCurso, {
+    path: ["finalCurso"],
+    message: "A data de fim deve ser maior ou igual à data de início",
+  })
+  .refine((data) => data.dataExpedido >= data.finalCurso, {
+    path: ["dataExpedido"],
+    message: "A data de expedição deve ser maior ou igual à data de fim",
+  });
 
 export type FormData = z.infer<typeof formSchema>;
